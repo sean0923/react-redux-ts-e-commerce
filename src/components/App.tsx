69010184b3a9be028b42b8e7bfbbd14ca5ea3890
Sean <iamseanhong@gmail.com>
2019-07-15 18:57:45 -0700

    Render shop page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import "./App.css";
+import './App.css';
 
-import { Home } from "./pages/Home";
+import { Home } from './pages/Home';
+import { Shop } from './pages/Shop';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route eaxct path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/shop" component={Shop} />
       </Switch>
     </BrowserRouter>
   );
