65fe7df90a6dd95ed81750c3942a93ad015772fb
Sean <iamseanhong@gmail.com>
2019-07-15 22:07:48 -0700

    Implement FormInput

--- src/components/pages/signInAndSignUp/SignIn.tsx
+++ src/components/pages/signInAndSignUp/SignIn.tsx
@@ -1,5 +1,9 @@
 import React, { FormEvent } from 'react';
 
+import { FormInput } from '../../common/FormInput';
+
+type InputType = React.ChangeEvent<HTMLInputElement>;
+
 function SignIn() {
   const [email, setEmail] = React.useState<string>('');
   const [password, setPassword] = React.useState<string>('');
@@ -17,15 +21,24 @@ function SignIn() {
       <span>Sign in with your email and password</span>
 
       <form onSubmit={handleSubmit}>
-        <input name="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <label htmlFor="email">Email</label>
-        <input
+        <FormInput
+          label="Email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e: InputType) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <FormInput
+          label="Password"
           name="password"
           type="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e: InputType) => {
+            setPassword(e.target.value);
+          }}
         />
-        <label htmlFor="password">Password</label>
         <input type="submit" value="Submit Form" />
       </form>
     </div>
