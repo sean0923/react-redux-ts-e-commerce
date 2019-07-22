7f6321e0c95894ff2a6cfcd92eb7e34d9e27e4b4
Sean <iamseanhong@gmail.com>
2019-07-22 00:11:13 -0700

    Checkout page header

--- src/components/pages/CheckoutPage.tsx
+++ src/components/pages/CheckoutPage.tsx
@@ -3,7 +3,25 @@ import React from 'react';
 function CheckoutPage() {
   return (
     <div>
-      <h1>Checkout Page</h1>
+      <div className="checkout-page">
+        <div className="checkout-header">
+          <div className="header-block">
+            <span>Product</span>
+          </div>
+          <div className="header-block">
+            <span>Description</span>
+          </div>
+          <div className="header-block">
+            <span>Quantity</span>
+          </div>
+          <div className="header-block">
+            <span>Price</span>
+          </div>
+          <div className="header-block">
+            <span>Remove</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
